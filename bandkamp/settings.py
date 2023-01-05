267bcad97abc8e2041f3fe586fd66c09b9cbfbfd
77joaovitor <henrique.joaovitor@gmail.com>
2023-01-05 13:52:35 -0300

    att

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -173,6 +173,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -181,5 +184,7 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOST = ['*']
 
 AUTH_USER_MODEL = "users.User"
