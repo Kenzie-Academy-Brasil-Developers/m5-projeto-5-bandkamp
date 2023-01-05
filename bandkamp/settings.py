77317d9145507b7d306d4e09b840c51cf1305894
João Vitor Henrique Gonçalves <103143992+77joaovitor@users.noreply.github.com>
2023-01-05 15:02:51 -0300

    Update settings.py

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -173,8 +173,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
@@ -185,6 +183,5 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-ALLOWED_HOST = ['*']
 
 AUTH_USER_MODEL = "users.User"
